12396daa3d60d4bc9743372ed5df7f7d15cf9667
Diego Canez <canezdiego@gmail.com>
2024-04-15 17:47:44 +0200

    feat: update roamlinks directly

--- mkdocs_frontmatter_plugin/plugin.py
+++ mkdocs_frontmatter_plugin/plugin.py
@@ -1,6 +1,6 @@
 from mkdocs.plugins import BasePlugin
 from mkdocs_frontmatter_plugin.config import FrontMatterConfig
-from mkdocs_roamlinks_plugin.plugin import ROAMLINK_RE
+from mkdocs_roamlinks_plugin.plugin import ROAMLINK_RE, RoamLinkReplacer
 import re
 
 class FrontMatterPlugin(BasePlugin[FrontMatterConfig]):
@@ -31,19 +31,21 @@ class FrontMatterPlugin(BasePlugin[FrontMatterConfig]):
                 if k not in self.config.exclude
             }
 
-        table = self.construct_table(front_matter_dict)
+        table = self.construct_table(front_matter_dict, config["docs_dir"], page.file.src_path)
 
         # Prepend the table to the Markdown content
         updated_markdown = table + markdown
 
         return updated_markdown
 
-    def construct_table(self, front_matter_dict):
+    def construct_table(self, front_matter_dict, base_docs_url, page_url):
         table = "| **Properties** |  |\n"
         table += "| --- | --- |\n"
         for key, value in front_matter_dict.items():
             # escape pipes in values unless it's a markdown link
-            if not re.match(ROAMLINK_RE, str(value)):
+            if re.match(ROAMLINK_RE, str(value)):
+                value = re.sub(ROAMLINK_RE, RoamLinkReplacer(base_docs_url, page_url), str(value))
+            else:
                 value = str(value).replace("|", "\\|")
             table += f"| {key} | {value} |\n"
         table += "\n"
