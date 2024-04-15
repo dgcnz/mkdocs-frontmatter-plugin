57103ae45499723380e0cba28530c1fcfc605ae9
Diego Canez <canezdiego@gmail.com>
2024-04-15 17:06:34 +0200

    feat: v0

---
+++ mkdocs_frontmatter_plugin/plugin.py
@@ -0,0 +1,39 @@
+from mkdocs.plugins import BasePlugin
+from mkdocs_frontmatter_plugin.config import FrontMatterConfig
+
+
+class FrontMatterPlugin(BasePlugin[FrontMatterConfig]):
+    supports_multiple_instances = True
+
+    # Initialize plugin
+    def on_config(self, config):
+        if not self.config.enabled:
+            return
+
+    def on_page_markdown(self, markdown, page, config, **kwargs):
+        if not self.config.enabled:
+            return
+
+        front_matter_dict = page.meta
+
+        # Construct table from front matter data
+        if self.config.attributes:
+            front_matter_dict = {
+                k: v
+                for k, v in front_matter_dict.items()
+                if k in self.config.attributes
+            }
+
+        table = self.construct_table(front_matter_dict)
+
+        # Prepend the table to the Markdown content
+        updated_markdown = table + markdown
+
+        return updated_markdown
+
+    def construct_table(self, front_matter_dict):
+        table = "| Key | Value |\n"
+        table += "| --- | --- |\n"
+        for key, value in front_matter_dict.items():
+            table += f"| {key} | {value} |\n"
+        return table
